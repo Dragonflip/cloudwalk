40763834e283e6c343ab5f2bf41acdcb0fa46254
jpmarques <jpmarquesdeoliveira@gmail.com>
2024-02-10 12:02:19 -0300

    inicializando projeto

---
+++ cloudwalk/db/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class Client(Base):
+    __tablename__ = 'clients'
+
+    user_id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    status = Column(String)
+    batch = Column(Integer)
+    credit_limit = Column(Integer)
+    interest_rate = Column(Integer)
+    denied_reason = Column(String)
+    denied_at = Column(DateTime)
+
+    loans = relationship("Loan", back_populates="client")
+
+
+class Loan(Base):
+    __tablename__ = 'loans'
+
+    loan_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('clients.user_id'))
+    created_at = Column(DateTime)
+    due_at = Column(DateTime)
+    paid_at = Column(DateTime)
+    status = Column(String)
+    loan_amount = Column(Float)
+    tax = Column(Float)
+    due_amount = Column(Float)
+    amount_paid = Column(Float)
+
+    client = relationship("Client", back_populates="loans")

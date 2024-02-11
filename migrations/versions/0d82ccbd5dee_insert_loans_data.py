bd8c402e5580fbaaf06ca9f142a91193fc5d0c68
jpmarques <jpmarquesdeoliveira@gmail.com>
2024-02-11 19:53:18 -0300

    ajustando para utilizar a engine do alembic

--- migrations/versions/0d82ccbd5dee_insert_loans_data.py
+++ migrations/versions/0d82ccbd5dee_insert_loans_data.py
@@ -10,8 +10,6 @@ import csv
 import os
 
 from alembic import op
-from sqlalchemy import create_engine
-from  cloudwalk.settings import Settings
 
 # revision identifiers, used by Alembic.
 revision: str = '0d82ccbd5dee'
@@ -21,14 +19,13 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    settings = Settings()    #type: ignore
     dirname = os.getcwd()
     filename = os.path.join(dirname, 'migrations/data/loans.csv')
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         header = next(reader)
         columns = ','.join(header)
-        conn = create_engine(settings.DATABASE_URL).raw_connection()
+        conn = op.get_bind().engine.raw_connection()
         cursor = conn.cursor()
         cmd = f'COPY loans({columns}) FROM STDIN WITH (FORMAT CSV, HEADER FALSE)'
         cursor.copy_expert(cmd, f)
--- migrations/versions/3168150c1208_insert_clients_data.py
+++ migrations/versions/3168150c1208_insert_clients_data.py
@@ -10,8 +10,6 @@ import os
 import csv
 
 from alembic import op
-from sqlalchemy import create_engine
-from  cloudwalk.settings import Settings
 
 # revision identifiers, used by Alembic.
 revision: str = '3168150c1208'
@@ -20,14 +18,13 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
-    settings = Settings()    #type: ignore
     dirname = os.getcwd()
     filename = os.path.join(dirname, 'migrations/data/clients.csv')
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         header = next(reader)
         columns = ','.join(header)
-        conn = create_engine(settings.DATABASE_URL).raw_connection()
+        conn = op.get_bind().engine.raw_connection()
         cursor = conn.cursor()
         cmd = f'COPY clients({columns}) FROM STDIN WITH (FORMAT CSV, HEADER FALSE)'
         cursor.copy_expert(cmd, f)

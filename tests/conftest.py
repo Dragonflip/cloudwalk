f45ebb42215a70073bae899f4bb41c9262692d2f
jpmarques <jpmarquesdeoliveira@gmail.com>
2024-02-14 16:11:33 -0300

    adding tests to api

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from cloudwalk.db.engine import get_session
+from cloudwalk.db.models import Base, Client
+from cloudwalk.app import app
+
+
+@pytest.fixture
+def session():
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.create_all(engine)
+    with Session() as session:
+        yield session
+        session.rollback()
+
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def approved_client(session):
+    client = Client(
+        status="approved",
+        batch=1,
+        credit_limit=20000,
+        interest_rate=20
+    )
+    session.add(client)
+    session.commit()
+    session.refresh(client)
+    return client
+
+
+@pytest.fixture
+def test_client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
---
+++ tests/test_e2e.py
@@ -0,0 +1,47 @@
+from sqlalchemy import select
+
+from cloudwalk.db.models import Client
+
+
+def test_read_root(test_client):
+    response = test_client.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"message": "pong"}
+
+
+def test_create_client(test_client):
+    response = test_client.post("/users/", json={
+        "status": "approved",
+        "batch": 1,
+        "credit_limit": 100000,
+        "interest_rate": 20
+    })
+
+    assert response.status_code == 201
+    assert response.json()["user_id"] == 1
+    assert response.json()["status"] == "approved"
+
+def test_update_client(test_client, approved_client,  session):
+    credit_limit = 100000
+    response = test_client.put(f"/users/{approved_client.user_id}/", 
+    json={
+        "status": "approved",
+        "batch": 1,
+        "credit_limit": credit_limit,
+        "interest_rate": 20
+    })
+
+    session.refresh(approved_client)
+
+    assert response.status_code == 200
+    assert approved_client.credit_limit == credit_limit
+
+def test_delete_client(test_client, approved_client,  session):
+    response = test_client.delete(f"/users/{approved_client.user_id}/")
+    deleted_client = session.scalar(
+        select(Client).where(
+            Client.user_id==approved_client.user_id
+        )
+    )
+    assert response.status_code == 200
+    assert deleted_client == None
